383d2db3cdf91a3893ef4eb8512dd156b5309bed
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-05-11 21:44:27 +0500

    изменено название языка программирования

--- axion/__main__.py
+++ axion/__main__.py
@@ -1,4 +1,4 @@
-# author https://github.com/MIrrox27/Axiom-Language
+# author https://github.com/MIrrox27/Axion-Language
 # __main__.py
 
 import sys
@@ -7,14 +7,14 @@ import _sitebuiltins
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from axiom.AxiomTokens import *
-from axiom.AxiomLexer import *
-from axiom.AxiomParser import *
-# from axiom.AxiomASTNodes import *
-from axiom.AxiomInterpreter import *
+from axion.AxionTokens import *
+from axion.AxionLexer import *
+from axion.AxionParser import *
+# from axion.AxionASTNodes import *
+from axion.AxionInterpreter import *
 
-from axiom.repl import repl
-from axiom.AxiomLicense import __axiom_license__
+from axion.repl import repl
+from axion.AxionLicense import __axion_license__
 
 
 
@@ -37,7 +37,7 @@ builtins.license = license
 
 
 def run_file(filename):
-    allowed_extensions = ('.axm', '.axi', '.ax')
+    allowed_extensions = ('.axn', '.axi', '.ax')
 
 
 
@@ -52,9 +52,9 @@ def run_file(filename):
     with open(path_license, 'r', encoding='utf-8') as f:
         axm_license = f.read()
 
-        if str(axm_license) != __axiom_license__ :
+        if str(axm_license) != __axion_license__ :
             #print(f'\n\n\n {axm_license}\n\n\n')
-            #print(f'\n\n\n {__axiom_license__}\n\n\n')
+            #print(f'\n\n\n {__axion_license__}\n\n\n')
             print('[ERROR] Invalid license: License has been modified')
             sys.exit(1)
 
@@ -75,17 +75,17 @@ def run_file(filename):
         sys.exit(1)
 
 
-    lexer = AxiomLexer(code)
-    parser = AxiomParser(lexer)
-    interpreter = AxiomInterpreter()
+    lexer = AxionLexer(code)
+    parser = AxionParser(lexer)
+    interpreter = AxionInterpreter()
 
     try:
-        while parser.current_token.type != AxiomTokenType.EOF:
+        while parser.current_token.type != AxionTokenType.EOF:
             # print(f"DEBUG: Current token before parse: {parser.current_token}")
             stmt = parser.parse_statement()
             interpreter.visit(stmt)
 
-        if parser.current_token.type != AxiomTokenType.EOF:
+        if parser.current_token.type != AxionTokenType.EOF:
             raise Exception("Parsing did not consume all tokens")
 
     except Exception as e:
@@ -102,8 +102,8 @@ def main():
         run_file(sys.argv[1])
 
         filename = sys.argv[1]
-        if not filename.endswith(('.ax', '.axiom', '.axi')):
-            print(f'Error: Only Axiom files (.ax, .axiom, .axi) can be executed.')
+        if not filename.endswith(('.ax', '.axion', '.axi')):
+            print(f'Error: Only Axion files (.ax, .axion, .axi) can be executed.')
             sys.exit(1)
 
     else:
