eeea5d88dbb267ae116f31784a2646366ca5c23b
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-05-13 16:40:56 +0500

    изменена структура и название языка

---
+++ axion/modules/LORMS/Messenger/AxionMessengerClient.py
@@ -0,0 +1,14 @@
+# author https://github.com/MIrrox27/Axion-Language
+# AxionMessengerClient.py
+
+from axion.modules.LORMS.Messenger.AxionMessenger import Messenger
+from axion.modules.LORMS.Messenger.AxionMessengerHandler import *
+
+
+
+class MessengerWebClient(MessengerHTTP): # для этой вариации библиотеки клиентом служит браузер пользователя
+    pass
+
+
+
+
