4cd60885dbb14fbb81d36f079568c1a9e4fb3248
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-05-14 20:28:57 +0500

    изменена структура и название языка

--- axiom/modules/web/AxiomWebClient.py
+++
@@ -1,8 +0,0 @@
-# author https://github.com/MIrrox27/Axiom
-
-from axiom.modules.web.AxiomWebModule import WebHandler
-
-
-class ClientModule(WebHandler):
-    def __init__(self):
-        pass
---
+++ axion/modules/web/AxionWebClient.py
@@ -0,0 +1,8 @@
+# author https://github.com/MIrrox27/Axion-Language
+
+#from axion.modules.web.AxionWebModule import WebHandler
+
+
+class ClientModule:
+    def __init__(self):
+        pass
